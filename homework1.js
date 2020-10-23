ee460a4ea2a9f08642fc67078a641036230c8a26
mrox <piotrmr20@gmail.com>
2020-10-23 02:47:11 +0200

    homework

---
+++ homework1.js
@@ -0,0 +1,28 @@
+// 1)	From years in array check for leap years [1974, 1900, 1985, 2000]
+// let years = [1974, 1900, 1985, 2000];
+// let year; 
+// for (let i = 0; i < years.length; i++) {
+//     year = years[i];
+
+// if ((year % 100 === 0 || year % 4 === 0) && year % 400 !== 0) {
+//     console.log(year);
+// }
+// }
+
+// 2)	Calculate factorial of 7.
+
+// let factorial = [1,2,3,4,5,6,7]
+// let output = 1; 
+// for (let i = 0; i < factorial.length; i++) { 
+//     output = output * factorial[i]; 
+
+// }
+// console.log("factorial of 7 is: ", output);
+
+// let factorial = 1;
+// let output = 1; 
+// for (let i = 1; i <= 7; i++) { 
+//     output = output * i; 
+    
+// }
+// console.log("factorial of 7 is: ", output);
